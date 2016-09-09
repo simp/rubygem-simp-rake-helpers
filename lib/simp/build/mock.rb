e9411b4e4d61b42f294e3b5d757728f14452e56c
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-09-08 21:35:25 -0400

    (SIMP-1465) Safety save
    SIMP-1465 #comment This is a temporary safety save.
    SIMP-1465 #comment It is being PRed for preview & discussion.
    SIMP-1465 #comment It should not be committed.
    SIMP-1465 #comment If this has been merged, something went very wrong.
    SIMP-1465 #comment I wish this was a haiku.
    
    Change-Id: Iabe5cd7f2e44a45717f8bca1f588452159bfe98a

---
+++ lib/simp/build/mock.rb
@@ -0,0 +1,244 @@
+require 'simp/rake/pkg'
+
+module Simp
+  module Simp::Build
+
+    # Interface to the {https://fedoraproject.org/wiki/Mock?rd=Subprojects/Mock Fedora mock} tool for building SIMP packages
+    # @note This Class requires the `mock` application to be installed.
+    class Simp::Build::Mock
+      include Simp
+
+      # Criticality levels
+      #
+      # * `:verbose` = more output from mock
+      # * `:normal`  = normal output from mock
+      # * `:quiet`   = skip output
+      # * `:silent`  = skip command && output
+      VERBOSE_LEVELS = [:silent,:quiet,:normal,:verbose]
+
+      # @!attribute [r]
+      #   Verbosity level for this mock instance
+      #   @return [Symbol] (default: `:normal`)
+      attr_reader   :verbose
+
+      # @!attribute [rw]
+      #   Absolute path to `mock` executable
+      #   @return [Boolean]
+      attr_accessor :mock_bin
+
+      # @!attribute [rw]
+      #   Path to mock's config directory
+      #   @note   This can be overriden using the environment variable `SIMP_BUILD_mock_config_dir`
+      #   @return [String] (default: `/etc/mock`)
+      attr_accessor :mock_config_dir
+
+      # @!attribute [rw]
+      #   Whether to run mock in 'offline' mode
+      #   @note   This can be overriden using the environment variable `SIMP_BUILD_mock_offline`
+      #   @return [Boolean] (default: `false`)
+      attr_accessor :mock_offline
+
+      # @!attribute [rw]
+      #   A unique String, used to keep the mock root exclusive to this instance.
+      #   @note   This can be overriden using the environment variable `SIMP_BUILD_mock_uniqueext`
+      #   @return [String] (default: '')
+      attr_accessor :uniqueext
+
+      # @!attribute [rw]
+      #   When true, `mock_init()` will automatically execute if needed during `mock_cmd`
+      #   @return [Boolean] (default: `true`)
+      attr_accessor :auto_init
+
+      # @param [String]  chroot
+      # @param [String]  uniqueext
+      # @param [Boolean] cleanup_after
+      def initialize(chroot, result_dir, uniqueext=nil, cleanup_after=false)
+        @chroot           = chroot
+        @uniqueext        = uniqueext || ENV['SIMP_BUILD_mock_uniqueext'] || ENV['USER']
+        @cleanup_after    = cleanup_after
+        @auto_init        = true
+        @mock_bin         = ENV['SIMP_BUILD_mock_bin']        || '/usr/bin/mock'
+        @mock_config_dir  = ENV['SIMP_BUILD_mock_config_dir'] || '/etc/mock'
+        @mock_offline     = ENV.fetch('SIMP_BUILD_mock_offline','no') =~ /^yes$/ ? true : false
+        @snapshot_release = ''
+        @result_dir       = result_dir # TODO: default to pwd?  require as param?
+        @verbose          = :normal
+      end
+
+
+      # Tests for the presence of the mock executable, mock configs, etc.
+      #
+      # @return [Boolean] `true` if mock environment is good
+      # @raise  [RuntimeError] raised with a specific error message if mock won't work.
+      def validate_mock_environment
+        unless File.executable?(@mock_bin)
+          raise(RuntimeError ,"Could not find mock on your system, exiting")
+        end
+
+        unless mock_configs.include?(@chroot)
+          raise(RuntimeError,
+            "Error: Invalid mock chroot provided.  " +
+            "Your choices are:\n  #{mock_configs.join("\n  ")}"
+          )
+        end
+
+        unless File.directory?(@result_dir)
+          raise(RuntimeError ,"result_dir must be a directory ('#{@result_dir}')")
+        end
+
+        unless @uniqueext.nil? || (@uniqueext.is_a?(String) && !@uniqueext.empty?)
+          raise RuntimeError, 'uniqueext must be nil or a non-empty String ' +
+                              %Q(("#{@uniqueext}" = #{@uniqueext.class}))
+        end
+
+        # TODO: This exhausts the original code's checks.  Should there be more?
+        true
+      end
+
+
+      # Constructs the boring part of the mock CLI string
+      # @returns [Array] beginning portion of the CLI invocation for mock
+      def mock_cmd
+        validate_mock_environment
+
+        cmd = ["#{@mock_bin}"]
+        cmd = cmd + ["--uniqueext=#{@uniqueext}"] if @uniqueext
+        cmd = cmd + ['--offline']                 if @mock_offline
+        cmd = cmd + ['--verbose'] if @verbose == :verbose
+        cmd = cmd + ['--quiet']   if @verbose == :quiet || @verbose == :silent
+
+        if @auto_init && !initialized?(cmd + ["--root=#{@chroot}"])
+          mock_init(cmd + ["--root=#{@chroot}"])
+        end
+
+        cmd = cmd + ['--no-cleanup-after'] unless @cleanup_after
+        cmd = cmd + [
+                      '--no-clean',
+                      "--resultdir=#{@result_dir}",
+                      '--disable-plugin=package_state',
+                      "--root=#{@chroot}",
+                     ]
+        cmd
+      end
+
+
+      # A simple test to see if the chroot is initialized.
+      # @param [String] _cmd An optional {#mock_cmd} String to use.  If this is not provided, a value will be obtained by running {#mock_cmd}.
+      # @returns [Boolean]
+      def initialized?(_cmd=nil)
+        cmd = _cmd || mock_cmd
+
+        verbose = :quiet
+        verbose = @verbose if (@verbose == :silent || @verbose == :verbose)
+        status = sh (cmd + ['--chroot', 'test -d /tmp &> /dev/null']), verbose
+        status.success?
+      end
+
+
+      # Initialize a mock root
+      # @param [String] _cmd An optional {#mock_cmd} String to use.  If this is not provided, a value will be obtained by running {#mock_cmd}.
+      def mock_init(_cmd=nil)
+        cmd = _cmd || mock_cmd
+
+        unless initialized?(cmd)
+          sh cmd + ["--root=#{@chroot}", '--init', "#{@uniqueext}"]
+        else
+          # Remove any old build cruft from the mock directory.
+          # This is kludgy but WAY faster than rebuilding them all, even with a cache.
+          # TODO: verify that this is still true
+          sh cmd + [
+                     "--root=#{@chroot}",
+                     '--chroot',
+                     "/bin/rm -rf /builddir/build/BUILDROOT /builddir/build/*/*"
+                   ]
+        end
+
+        # Install useful stock packages
+        if ENV.fetch( 'SIMP_BUILD_mock_extras', 'yes' ) == 'yes'
+          pkgs     = ['openssl', 'openssl-devel']
+          env_pkgs = ENV.fetch('SIMP_RAKE_mock_pkgs','')
+          pkgs     = pkgs + env_pkgs.split(',') unless env_pkgs.empty?
+          pkgs.each { |pkg| sh (cmd + ['--install', "#{pkg}"]) }
+        end
+      end
+
+
+      # Set the verbosity level
+      # @param v [Symbol] level
+      def verbose=(v)
+        unless VERBOSE_LEVELS.include?(v)
+          raise ArgumentError, "`verbose=` argument must be one of [#{VERBOSE_LEVELS.join(',')}]"
+        end
+        @verbose = v
+      end
+
+      # @parameter *arg arguments(s) to pass to `mock`
+      def run(*arg)
+        raise NotImplementedError, 'TODO run(*arg)!'
+      end
+
+      # Purge the chroot tree.
+      def clean
+        sh mock_cmd + ['--clean']
+      end
+
+      # Completely remove the specified chroot or cache dir or all of the chroot and cache.
+      def scrub
+        sh mock_cmd + ['--scrub=all']
+      end
+
+      # Do a yum install PACKAGE inside the chroot. No 'clean' is performed
+      # @param [*String]
+      def install( *package )
+        sh mock_cmd + ['--install'] + package
+      end
+
+      def update( *package )
+        sh mock_cmd + ['--update'] + package
+      end
+
+      def copyin( *src_path, dest_path )
+        sh mock_cmd + ['--copyin'] + src_path + [dest_path]
+      end
+
+      def copyout( *src_path, dest_path )
+        sh mock_cmd + ['--copyout'] + src_path + [dest_path]
+      end
+
+
+      # ------------------------------------------------------------------------------
+      # helper methods
+      # ------------------------------------------------------------------------------
+
+      # Return a list of all of the mock configs available on the system.
+      # @return [Array] A list of all the mock configs available on the system.
+      def mock_configs
+        @mock_configs ||= Dir.glob(File.join(@mock_config_dir,'*.cfg')).sort.map{ |x| x = File.basename(x,'.cfg')}
+      end
+
+
+      # Execute a shell command
+      #
+      # This behaves more or less like Rake's `sh` method, but with hooks that
+      # could be redirected for logging.
+      #
+      # @param [Array] cmd Command to execute, split into Array elements (like `Kernel#spawn`)
+      def sh(cmd, _verbose=nil)
+        verbose = _verbose || @verbose
+
+        # TODO: ATTN Logger fans, place your hooks here:
+        out = :out
+        err = :err
+        if verbose == :silent || verbose == :quiet
+          out = :close
+          err = :close
+        end
+        puts "== #{cmd.join(' ')}" unless verbose == :silent
+
+        pid = spawn(*cmd, :out => out, :err => err )
+        _pid, status = Process.wait2(pid)
+        return status
+      end
+    end
+  end
+end
